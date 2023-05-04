9729d0fe9d190e33e1f3e3f9661a864c981c6a21
Oladipo George <oladipo.george@erlang-solutions.com>
2023-05-04 20:47:37 +0100

    initial starting point

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,41 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
+//Layout
+import { ApplicationComponent } from './layout/application/application.component'
+import { AuthComponent } from './layout/auth/auth.component'
+// Auth Pages
+import { LoginComponent } from './pages/auth/login/login.component'
+import { RegisterComponent } from './pages/auth/register/register.component'
+// App Pages
+import { DashboardComponent } from './pages/application/dashboard/dashboard.component'
+//Error Pages
+import { NotFoundComponent } from './pages/error/not-found/not-found.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  //Site routes goes here
+  {
+    path: '',
+    component: ApplicationComponent,
+    children: [
+      { path: 'dashboard', component: DashboardComponent, pathMatch: 'full' },
+    ],
+  },
+
+  // App routes goes here here
+  {
+    path: 'auth',
+    component: AuthComponent,
+    children: [
+      { path: 'login', component: LoginComponent },
+      { path: 'register', component: RegisterComponent },
+    ],
+  },
+  // otherwise redirect to home
+  { path: '**', component: NotFoundComponent },
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,43 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { HeaderComponent } from './components/partials/header/header.component'
+import { FooterComponent } from './components/partials/footer/footer.component'
+import { SidebarComponent } from './components/partials/sidebar/sidebar.component'
+import { ApplicationComponent } from './layout/application/application.component'
+import { AuthComponent } from './layout/auth/auth.component'
+import { LoginComponent } from './pages/auth/login/login.component'
+import { RegisterComponent } from './pages/auth/register/register.component'
+import { DashboardComponent } from './pages/application/dashboard/dashboard.component'
+import { NotFoundComponent } from './pages/error/not-found/not-found.component'
+import { MaintenanceComponent } from './pages/error/maintenance/maintenance.component'
+import { ButtonComponent } from './components/events/button/button.component'
+import { TextComponent } from './components/input/text/text.component'
+import { TextareaComponent } from './components/input/textarea/textarea.component'
+import { CheckboxComponent } from './components/input/checkbox/checkbox.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent,
+    ApplicationComponent,
+    AuthComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent,
+    NotFoundComponent,
+    MaintenanceComponent,
+    ButtonComponent,
+    TextComponent,
+    TextareaComponent,
+    CheckboxComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
